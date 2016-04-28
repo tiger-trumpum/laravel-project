ce23573a585d9bfa1786902c531ac1b2e232137d
tiger-trumpum <tigran_1993@mail.ru>
2016-04-28 16:00:35 +0300

    Update create.blade.php

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -6,7 +6,7 @@
     <div class="col-md-6"><h2>Leave a message</h2></div>
     <div class="col-md-6" align="right"> <img width="120" height="120" src="../images/{{$name}}.jpg" alt="{{$name}}"> </div>
     <hr />
-    {!!Form::open(array('id' => 'popupfrmcallorder', 'url' => 'messages', 'files' => true)) !!}
+    {!!Form::open(array('url' => 'messages', 'files' => true)) !!}
         @include('messages.form', ['submitButtonText' => 'Add Message'])
     {!! Form::close() !!}
     @include('errors.list')
